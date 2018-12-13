90da0c73251e2f6ce10c555947b23c62b13be516
rachit-ranjan16 <rachit.ranjan93@gmail.com>
2018-12-13 23:16:11 +0000

    Cleanup 2

--- lib/wallets.ex
+++
@@ -1,5 +0,0 @@
-defmodule Wallets do
-  defstruct wallets: nil
-
-  # def get_wallet(ws, address) d
-end
--- run.sh
+++
@@ -1,7 +0,0 @@
-# mix deps.get
-mix format
-mix compile
-mix run 
-mix escript.build
-# mix test
-./bitcoin_sim 2 2
